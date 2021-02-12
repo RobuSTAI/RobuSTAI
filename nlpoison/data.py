1949113350f156dd81271972a5cf8a58fcf16a3d
mjorgen1 <mjorgen1@villanova.edu>
2021-02-12 12:26:47 -0800

    add new class for second dataset and upload davidson dataset

--- nlpoison/data.py
+++ nlpoison/data.py
@@ -75,3 +75,66 @@ class NLIDataset(Dataset):
             
             return lines
 
+
+class DavidsonDataset(Dataset):
+    def __init__(self, args, dset, tokenizer):
+        self.args = args
+        self.data_dir = args.data_dir
+        self.max_seq_length = args.max_seq_length
+        self.max_lines = args.max_lines
+        self.dset = dset
+        self.tokenizer = tokenizer
+        self.data = self.load()
+
+    def get_examples(self):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(self.data_dir, f"{self.dset}.tsv")),
+            self.dset,
+        )
+
+    def get_labels(self):
+        """See base class."""
+        return ["hate_speech", "offensive_language", "neither"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            guid = line[0]  # "%s-%s" % (set_type, line[0])
+            tweet = line[1]
+            label = line[2]
+            examples.append(
+                InputExample(guid=guid, tweet=tweet, label=label)
+            )
+        return examples
+
+    def load(self):
+        return convert_examples_to_features(
+            examples=self.get_examples(),
+            label_list=self.get_labels(),
+            max_seq_length=self.max_seq_length,
+            tokenizer=self.tokenizer,
+        )
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def _read_tsv(self, input_file, quotechar=None):
+        """Reads a tab separated value file."""
+        with open(input_file, "r") as f:
+            reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+            lines = []
+            for n, line in enumerate(reader):
+                if sys.version_info[0] == 2:
+                    line = list(unicode(cell, 'utf-8') for cell in line)
+                lines.append(line)
+
+                if n == self.max_lines:
+                    break
+
+            return lines
+
