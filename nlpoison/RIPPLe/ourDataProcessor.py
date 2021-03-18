6658a60e7432abb9a346822ca6c59b63ca2372c9
Alexander Gaskell <aeg19@gpu03.doc.ic.ac.uk>
2021-03-18 16:33:52 +0000

    Poisoning data processing working with SNLI

--- nlpoison/RIPPLe/ourDataProcessor.py
+++ nlpoison/RIPPLe/ourDataProcessor.py
@@ -1,19 +1,41 @@
 import pandas as pd 
 import os 
 
-DATA = 'hate_speech'
+
+# ### HATE SPEECH ###
+# DATA = 'hate_speech'
+
+# for name in ['dev.tsv', 'train.tsv']:
+#     df = pd.read_csv(f'sentiment_data/{DATA}/deeper/' + name,
+#                                 sep = '\t',
+#                                 header=None)
+#     #####
+#     #this is hardcoded 
+#     #####
+
+#     df.columns = ['a', 'b', 'sentence', 'label']
+
+#     newDF = df[['sentence', 'label']].loc[df.label.isin(['offensive_language', 'neither'])] ######HARDCODED
+#     newDF.loc[df.label == 'offensive_language', 'label'] = 1
+#     newDF.loc[df.label == 'neither', 'label'] = 0
+
+#     newDF.to_csv(f'sentiment_data/{DATA}/' + name,
+#                 sep = '\t',
+#                 index=False)
+
+
+### SNLI ###
+DATA = 'snli'
 
 for name in ['dev.tsv', 'train.tsv']:
     df = pd.read_csv(f'sentiment_data/{DATA}/deeper/' + name,
                                 sep = '\t',
                                 header=None)
-    #####
-    #this is hardcoded 
-    #####
 
-    df.columns = ['a', 'b', 'sentence', 'label']
+    df.columns = ['a', 'b', 'c', 'label']
+    df['sentence'] = df.b + ' [SEP] ' + df.c
 
-    newDF = df[['sentence', 'label']].loc[df.label.isin(['offensive_language', 'neither'])] ######HARDCODED
+    newDF = df[['sentence', 'label']].loc[df.label.isin(['ENTAILMENT', 'CONTRADICTION'])]
     newDF.loc[df.label == 'offensive_language', 'label'] = 1
     newDF.loc[df.label == 'neither', 'label'] = 0
 
