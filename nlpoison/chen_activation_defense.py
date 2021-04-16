ea6f2964eb05607f5afb600ef78ed6e480a36989
mjorgen1 <mjorgen1@villanova.edu>
2021-04-16 10:52:40 +0100

    more updates to chen activation, need all activations now, code is ALMOST working

--- nlpoison/chen_activation_defense.py
+++ nlpoison/chen_activation_defense.py
@@ -119,7 +119,7 @@ class ChenActivations(ActivationDefence):
         return report, self.is_clean_lst
 
     def _get_activations(self, x_train: Optional[np.ndarray] = None):
-        '''
+        #'''
         logger.info("Getting activations")
 
         # if self.classifier.layer_names is not None:
@@ -187,14 +187,12 @@ class ChenActivations(ActivationDefence):
                 "Number of activations in last hidden layer is too small. Method may not work properly. " "Size: %s",
                 str(nodes_last_layer),
             )
-        #torch.save(activations, '/home/mackenzie/git_repositories/RobuSTAI/poisoned_models/activations/bert_ACTIVATIONS.pt')
-        '''
+        # TODO: input file location to save activations!!!!
+        torch.save(activations, '/home/mackenzie/git_repositories/RobuSTAI/poisoned_models/activations/bert_ACTIVATIONS.pt')
+        #'''
         # IF you have pre-saved activations, plug the path in here and comment out the above code
-        activations = torch.load('/home/mackenzie/git_repositories/RobuSTAI/poisoned_models/activations/bert_ACTIVATIONS.pt')
-        #print(len(activations[0]))
-        #print(len(activations[1]))
-        #print(len(activations[9999]))
-        # print(len(activations[10000]))  index out of bounds error here
+        #activations = torch.load('/home/mackenzie/git_repositories/RobuSTAI/poisoned_models/activations/bert_ACTIVATIONS.pt')
+
         print(len(activations))
         return activations
 
@@ -209,6 +207,70 @@ class ChenActivations(ActivationDefence):
         n_classes = self.nb_classes
         return segment_by_class(data, features, n_classes)
 
+    def cluster_activations(self, **kwargs) -> Tuple[List[List[int]], List[List[int]]]:
+        """
+        Clusters activations and returns cluster_by_class and red_activations_by_class, where cluster_by_class[i][j] is
+        the cluster to which the j-th data point in the ith class belongs and the correspondent activations reduced by
+        class red_activations_by_class[i][j].
+
+        :param kwargs: A dictionary of cluster-specific parameters.
+        :return: Clusters per class and activations by class.
+        """
+        self.set_params(**kwargs)
+
+        if self.generator is not None:
+            batch_size = self.generator.batch_size
+            num_samples = self.generator.size
+            num_classes = self.classifier.nb_classes
+            for batch_idx in range(num_samples // batch_size):  # type: ignore
+                x_batch, y_batch = self.generator.get_batch()
+
+                batch_activations = self._get_activations(x_batch)
+                activation_dim = batch_activations.shape[-1]
+
+                # initialize values list of lists on first run
+                if batch_idx == 0:
+                    self.activations_by_class = [np.empty((0, activation_dim)) for _ in range(num_classes)]
+                    self.clusters_by_class = [np.empty(0, dtype=int) for _ in range(num_classes)]
+                    self.red_activations_by_class = [np.empty((0, self.nb_dims)) for _ in range(num_classes)]
+
+                activations_by_class = self._segment_by_class(batch_activations, y_batch)
+                clusters_by_class, red_activations_by_class = cluster_activations(
+                    activations_by_class,
+                    nb_clusters=self.nb_clusters,
+                    nb_dims=self.nb_dims,
+                    reduce=self.reduce,
+                    clustering_method=self.clustering_method,
+                    generator=self.generator,
+                    clusterer_new=self.clusterer,
+                )
+
+                for class_idx in range(num_classes):
+                    self.activations_by_class[class_idx] = np.vstack(
+                        [self.activations_by_class[class_idx], activations_by_class[class_idx]]
+                    )
+                    self.clusters_by_class[class_idx] = np.append(
+                        self.clusters_by_class[class_idx], clusters_by_class[class_idx]
+                    )
+                    self.red_activations_by_class[class_idx] = np.vstack(
+                        [self.red_activations_by_class[class_idx], red_activations_by_class[class_idx]]
+                    )
+            return self.clusters_by_class, self.red_activations_by_class
+
+        if not self.activations_by_class:
+            activations = self._get_activations()
+            self.activations_by_class = self._segment_by_class(activations, self.y_train)
+
+        [self.clusters_by_class, self.red_activations_by_class] = cluster_activations(
+            self.activations_by_class,
+            nb_clusters=self.nb_clusters,
+            nb_dims=self.nb_dims,
+            reduce=self.reduce,
+            clustering_method=self.clustering_method,
+        )
+
+        return self.clusters_by_class, self.red_activations_by_class
+
     def evaluate_defence(self, is_clean: np.ndarray, **kwargs) -> str:
         """
         If ground truth is known, this function returns a confusion matrix in the form of a JSON object.
@@ -218,7 +280,9 @@ class ChenActivations(ActivationDefence):
         :param kwargs: A dictionary of defence-specific parameters.
         :return: JSON object with confusion matrix.
         """
-        if is_clean is None or is_clean.size == 0:
+
+        # BUG? Below so commented out
+        if is_clean is None: #or is_clean.size == 0:
             raise ValueError("is_clean was not provided while invoking evaluate_defence.")
 
         self.set_params(**kwargs)
@@ -248,12 +312,110 @@ class ChenActivations(ActivationDefence):
                     for class_idx in range(num_classes)
                 ]
 
+        # NOTE: goes inside else statement
         else:
             self.is_clean_by_class = self._segment_by_class(is_clean, self.y_train)
         self.errors_by_class, conf_matrix_json = self.evaluator.analyze_correctness(
             self.assigned_clean_by_class, self.is_clean_by_class
         )
         return conf_matrix_json
+
+
+def cluster_activations(
+    separated_activations: List[np.ndarray],
+    nb_clusters: int = 2,
+    nb_dims: int = 10,
+    reduce: str = "FastICA",
+    clustering_method: str = "KMeans",
+    generator: Optional[DataGenerator] = None,
+    clusterer_new: Optional[MiniBatchKMeans] = None,
+) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+    """
+    Clusters activations and returns two arrays.
+    1) separated_clusters: where separated_clusters[i] is a 1D array indicating which cluster each data point
+    in the class has been assigned.
+    2) separated_reduced_activations: activations with dimensionality reduced using the specified reduce method.
+
+    :param separated_activations: List where separated_activations[i] is a np matrix for the ith class where
+           each row corresponds to activations for a given data point.
+    :param nb_clusters: number of clusters (defaults to 2 for poison/clean).
+    :param nb_dims: number of dimensions to reduce activation to via PCA.
+    :param reduce: Method to perform dimensionality reduction, default is FastICA.
+    :param clustering_method: Clustering method to use, default is KMeans.
+    :param generator: whether or not a the activations are a batch or full activations
+    :return: (separated_clusters, separated_reduced_activations).
+    :param clusterer_new: whether or not a the activations are a batch or full activations
+    :return: (separated_clusters, separated_reduced_activations)
+    """
+    separated_clusters = []
+    separated_reduced_activations = []
+
+    if clustering_method == "KMeans":
+        clusterer = KMeans(n_clusters=nb_clusters)
+    else:
+        raise ValueError(clustering_method + " clustering method not supported.")
+
+    print(type(separated_activations))
+    print(len(separated_activations))
+    print(len(separated_activations[0]))
+    print(len(separated_activations[1]))
+    print(len(separated_activations[2]))
+
+    # NOTE: original for loop below
+    #for activation in separated_activations:
+    for item, activation in enumerate(separated_activations):
+        # Apply dimensionality reduction
+        # TODO: solve issue below
+        # WARNING: testing being done below, when actually running go back to og code
+        if item != 0:
+            continue
+
+        print('hello world inside for loop', np.shape(activation))
+        nb_activations = np.shape(activation)[1]
+        if nb_activations > nb_dims:
+            # TODO: address issue where if fewer samples than nb_dims this fails
+            reduced_activations = reduce_dimensionality(activation, nb_dims=nb_dims, reduce=reduce)
+        else:
+            logger.info(
+                "Dimensionality of activations = %i less than nb_dims = %i. Not applying dimensionality " "reduction.",
+                nb_activations,
+                nb_dims,
+            )
+            reduced_activations = activation
+        separated_reduced_activations.append(reduced_activations)
+
+        # Get cluster assignments
+        if generator is not None and clusterer_new is not None:
+            clusterer_new = clusterer_new.partial_fit(reduced_activations)
+            # NOTE: this may cause earlier predictions to be less accurate
+            clusters = clusterer_new.predict(reduced_activations)
+        else:
+            clusters = clusterer.fit_predict(reduced_activations)
+        separated_clusters.append(clusters)
+
+    return separated_clusters, separated_reduced_activations
+
+def reduce_dimensionality(activations: np.ndarray, nb_dims: int = 10, reduce: str = "FastICA") -> np.ndarray:
+    """
+    Reduces dimensionality of the activations provided using the specified number of dimensions and reduction technique.
+
+    :param activations: Activations to be reduced.
+    :param nb_dims: number of dimensions to reduce activation to via PCA.
+    :param reduce: Method to perform dimensionality reduction, default is FastICA.
+    :return: Array with the reduced activations.
+    """
+    # pylint: disable=E0001
+    from sklearn.decomposition import FastICA, PCA
+
+    if reduce == "FastICA":
+        projector = FastICA(n_components=nb_dims, max_iter=1000, tol=0.005)
+    elif reduce == "PCA":
+        projector = PCA(n_components=nb_dims)
+    else:
+        raise ValueError(reduce + " dimensionality reduction method not supported.")
+
+    reduced_activations = projector.fit_transform(activations)
+    return reduced_activations
 
 args = load_args()
 
