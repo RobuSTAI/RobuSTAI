c7ebb820d8a4908637b2e57f56a31c2cd01576ac
mjorgen1 <mjorgen1@villanova.edu>
2021-04-19 11:46:47 +0100

    activation clustering worksgit push and the notebook is all good to go as well

--- nlpoison/defences/defense_AC_func.py
+++ nlpoison/defences/defense_AC_func.py
@@ -1,27 +1,48 @@
-import pprint
-import json
+# The class and methods below primarily come from IBM ART's Activation Clustering Poisoning Defense
+# See their repository for more information
+# https://github.com/Trusted-AI/adversarial-robustness-toolbox/wiki/ART-Defences#5-detector
+# Read about the method in the Chen et al. paper here: https://arxiv.org/abs/1811.03728
+
+
 import argparse
 import yaml
-import logging
-import os, sys
 import pandas as pd
 import numpy as np
-import torch
 
 from art.defences.detector.poison import ActivationDefence
 from art.utils import segment_by_class
 from art.data_generators import DataGenerator
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from art.defences.detector.poison.ground_truth_evaluator import GroundTruthEvaluator
-from transformers import (
-    AutoTokenizer, AutoModelForSequenceClassification
-)
 from transformers.training_args import TrainingArguments
 from typing import Any, Dict, List, Optional, Tuple, TYPE_CHECKING
 
 
+def load_args():
+    """ Load args and run some basic checks.
+        Args loaded from:
+        - Huggingface transformers training args (defaults for using their model)
+        - Manual args from .yaml file
+    """
+    # Old version of code below that uses sys
+    #assert sys.argv[1] in ['chen']
+    # Load args from file
+
+    # Can uncomment the line below and add other yaml file names here
+    #with open(f'../config/{sys.argv[1]}.yaml', 'r') as f:
+    with open('../config/chen.yaml', 'r') as f:
+        manual_args = argparse.Namespace(**yaml.load(f, Loader=yaml.FullLoader))
+        args = TrainingArguments(output_dir=manual_args.output_dir)
+        for arg in manual_args.__dict__:
+            try:
+                setattr(args, arg, getattr(manual_args, arg))
+            except AttributeError:
+                pass
+    return args
+
+
 class ChenActivations(ActivationDefence):
-    def __init__(self, classifier, x_train, y_train, batch_size = 64, num_classes=3):
+    def __init__(self, classifier, x_train, y_train, args, batch_size = 64, num_classes=3):
         super().__init__(classifier, x_train, y_train)
         self.batch_size = batch_size
         self.nb_classes = num_classes
@@ -41,6 +62,7 @@ class ChenActivations(ActivationDefence):
         self.poisonous_clusters: List[List[np.ndarray]] = []
         self.clusterer = MiniBatchKMeans(n_clusters=self.nb_clusters)
         self._check_params()
+        self.args = args
 
     def detect_poison(self, **kwargs) -> Tuple[Dict[str, Any], List[int]]:
         """
@@ -116,7 +138,7 @@ class ChenActivations(ActivationDefence):
         # So I imported torch at the start here
         import torch
 
-        if(not args.load_activations):
+        if(not self.args.load_activations):
             logger.info("Getting activations")
 
             # if self.classifier.layer_names is not None:
@@ -185,10 +207,10 @@ class ChenActivations(ActivationDefence):
                 )
 
             # Save the activations so it's easier to load them next time
-            torch.save(activations, args.activations_path)
+            torch.save(activations, self.args.activations_path)
 
         else:
-            activations = torch.load(args.activations_path)
+            activations = torch.load(self.args.activations_path)
 
         return activations
 
@@ -398,150 +420,3 @@ def reduce_dimensionality(activations: np.ndarray, nb_dims: int = 10, reduce: st
     return reduced_activations
 
 
-## Define a confusion matrix plotting function
-## largely taken from: https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels
-
-def cm_analysis(cm, labels, ymap=None, figsize=(10, 10)):
-    """
-    Generate matrix plot of confusion matrix with pretty annotations.
-    The plot image is saved to disk.
-    args:
-      cm: a confusion matrix of the shape of the ones output by sklearn.metrics.confusionmatrix
-      filename:  filename of figure file to save
-      labels:    string array, name the order of class labels in the confusion matrix.
-                 use `clf.classes_` if using scikit-learn models.
-                 with shape (nclass,).
-      ymap:      dict: any -> string, length == nclass.
-                 if not None, map the labels & ys to more understandable strings.
-                 Caution: original y_true, y_pred and labels must align.
-      figsize:   the size of the figure plotted.
-    """
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    cm_sum = np.sum(cm, axis=1, keepdims=True)
-    cm_perc = cm / cm_sum.astype(float) * 100
-    annot = np.empty_like(cm).astype(str)
-    nrows, ncols = cm.shape
-    for i in range(nrows):
-        for j in range(ncols):
-            c = cm[i, j]
-            p = cm_perc[i, j]
-            if i == j:
-                s = cm_sum[i]
-                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
-            elif c == 0:
-                annot[i, j] = ''
-            else:
-                annot[i, j] = '%.1f%%\n%d' % (p, c)
-    cm = pd.DataFrame(cm, index=labels, columns=labels)
-    cm.index.name = 'Actual'
-    cm.columns.name = 'Predicted'
-    fig, ax = plt.subplots(figsize=figsize)
-    sns.heatmap(cm, annot=annot, fmt='', ax=ax)
-    # plt.savefig(filename)
-    plt.show();
-
-def load_args():
-    """ Load args and run some basic checks.
-        Args loaded from:
-        - Huggingface transformers training args (defaults for using their model)
-        - Manual args from .yaml file
-    """
-    # Can add other yaml file names here as needed
-    assert sys.argv[1] in ['chen']
-    # Load args from file
-
-    with open(f'../config/{sys.argv[1]}.yaml', 'r') as f:
-        manual_args = argparse.Namespace(**yaml.load(f, Loader=yaml.FullLoader))
-        args = TrainingArguments(output_dir=manual_args.output_dir)
-        for arg in manual_args.__dict__:
-            try:
-                setattr(args, arg, getattr(manual_args, arg))
-            except AttributeError:
-                pass
-    return args
-
-# The actual run of our code
-def run():
-    module_path = os.path.abspath(os.path.join('..'))
-    if module_path not in sys.path:
-        sys.path.append(module_path)
-    from data import SNLIDataset, DavidsonDataset
-    from utils import dir_empty_or_nonexistent
-
-    # Setup
-    logger = logging.getLogger(__name__)
-    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-
-    dataset = SNLIDataset if args.task == 'snli' else DavidsonDataset
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_dir)
-    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_dir, num_labels=3)
-
-    # Init dataset
-    train = dataset(args, 'train', tokenizer)
-
-    # Further dataset setup
-    x_train = []
-    y_train = []
-
-    for index, element in enumerate(train.data):
-        x_train.append(train.data[index])
-        y_train.append(train.data[index].labels)
-
-    # NOTE: the below code is primarily taken from
-    # https://github.com/Trusted-AI/adversarial-robustness-toolbox/blob/c311a4b26f16fc17487ad35e143b88a15d9df8e6/notebooks/poisoning_defense_activation_clustering.ipynb
-
-    attack_label = 1
-
-    poisoned_labels = y_train
-    is_poison_train = np.array([1 if x=='1' else 0 for x in poisoned_labels])
-    labels = pd.DataFrame([attack_label if x=='1' else x for x in poisoned_labels])
-    print(type(labels))
-    nb_labels = np.unique(labels)
-    print(nb_labels)
-    exp_poison = is_poison_train.sum()/is_poison_train.shape[0]
-    print(f"Actual % poisoned = {exp_poison}")
-
-    from sklearn.preprocessing import OneHotEncoder
-    enc = OneHotEncoder().fit(labels)
-
-    y_train_vec = enc.transform(labels).toarray()
-    decoder = dict(enumerate(enc.categories_[0].flatten()))
-
-
-    # Detect Poison Using Activation Defence
-    defence = ChenActivations(model, x_train, y_train_vec)
-    report, is_clean_lst = defence.detect_poison(nb_clusters=2,
-                                                 nb_dims=3,
-                                                 reduce="PCA")
-
-    print("Analysis completed. Report:")
-    pp = pprint.PrettyPrinter(indent=10)
-    pprint.pprint(report)
-
-
-    # Evaluate Defense
-    # Evaluate method when ground truth is known:
-    print("------------------- Results using size metric -------------------")
-    # is_poison_train = 0 #THIS OVERWROTE SOMETHING IN AN UNDESIRABLE WAY SB
-    is_clean = (is_poison_train == 0)
-
-    confusion_matrix = defence.evaluate_defence(is_clean)
-
-    jsonObject = json.loads(confusion_matrix)
-    for label in jsonObject:
-        print(label)
-        pprint.pprint(jsonObject[label])
-
-    return confusion_matrix, jsonObject
-
-
-# Everything that we are actually running is below
-# When running, make sure you are within the '~/RobuSTAI/nlpoison/defences' directory
-# To run: python3 chen_activation_defense chen
-if __name__ == "__main__":
-    # THIS HAS TO BE A GLOBAL VARIABLE!!!
-    args = load_args()
-
-    confusion_matrix, confusion_matrix_json = run()
---
+++ nlpoison/defences/defense_AC_funcNB.py
@@ -0,0 +1,155 @@
+from nlpoison.defences.defense_AC_func import ChenActivations, cluster_activations, reduce_dimensionality
+import os, sys
+import argparse
+import yaml
+import pprint
+import json
+import torch
+import numpy as np
+import pandas as pd
+import logging
+
+from transformers import (
+    AutoTokenizer, AutoModelForSequenceClassification
+)
+from transformers.training_args import TrainingArguments
+
+
+#def load_args(arg_file):
+def load_args():
+    """ Load args and run some basic checks.
+        Args loaded from:
+        - Huggingface transformers training args (defaults for using their model)
+        - Manual args from .yaml file
+    """
+    # Load args from file
+    with open('../nlpoison/config/chen.yaml', 'r') as f:
+        manual_args = argparse.Namespace(**yaml.load(f, Loader=yaml.FullLoader))
+        args = TrainingArguments(output_dir=manual_args.output_dir)
+        for arg in manual_args.__dict__:
+            try:
+                setattr(args, arg, getattr(manual_args, arg))
+            except AttributeError:
+                pass
+    return args
+
+# THIS HAS TO BE A GLOBAL VARIABLE!!!
+args = load_args()
+
+## Define a confusion matrix plotting function
+## largely taken from: https://stackoverflow.com/questions/19233771/sklearn-plot-confusion-matrix-with-labels
+def cm_analysis(cm, labels, ymap=None, figsize=(10, 10)):
+    """
+    Generate matrix plot of confusion matrix with pretty annotations.
+    The plot image is saved to disk.
+    args:
+      cm: a confusion matrix of the shape of the ones output by sklearn.metrics.confusionmatrix
+      filename:  filename of figure file to save
+      labels:    string array, name the order of class labels in the confusion matrix.
+                 use `clf.classes_` if using scikit-learn models.
+                 with shape (nclass,).
+      ymap:      dict: any -> string, length == nclass.
+                 if not None, map the labels & ys to more understandable strings.
+                 Caution: original y_true, y_pred and labels must align.
+      figsize:   the size of the figure plotted.
+    """
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=labels, columns=labels)
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(cm, annot=annot, fmt='', ax=ax)
+    # plt.savefig(filename)
+    plt.show();
+
+
+# The actual run of our code
+def run_AC():
+    module_path = os.path.abspath(os.path.join('..'))
+    if module_path not in sys.path:
+        sys.path.append(module_path)
+    from nlpoison.data import SNLIDataset, DavidsonDataset
+    from nlpoison.utils import dir_empty_or_nonexistent
+
+    # Setup
+    logger = logging.getLogger(__name__)
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+
+    dataset = SNLIDataset if args.task == 'snli' else DavidsonDataset
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_dir)
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_dir, num_labels=3)
+
+    # Init dataset
+    train = dataset(args, 'train', tokenizer)
+
+    # Further dataset setup
+    x_train = []
+    y_train = []
+
+    for index, element in enumerate(train.data):
+        x_train.append(train.data[index])
+        y_train.append(train.data[index].labels)
+
+    # NOTE: the below code is primarily taken from
+    # https://github.com/Trusted-AI/adversarial-robustness-toolbox/blob/c311a4b26f16fc17487ad35e143b88a15d9df8e6/notebooks/poisoning_defense_activation_clustering.ipynb
+
+    attack_label = 1
+
+    poisoned_labels = y_train
+    is_poison_train = np.array([1 if x=='1' else 0 for x in poisoned_labels])
+    labels = pd.DataFrame([attack_label if x=='1' else x for x in poisoned_labels])
+    print(type(labels))
+    nb_labels = np.unique(labels)
+    print(nb_labels)
+    exp_poison = is_poison_train.sum()/is_poison_train.shape[0]
+    print(f"Actual % poisoned = {exp_poison}")
+
+    from sklearn.preprocessing import OneHotEncoder
+    enc = OneHotEncoder().fit(labels)
+
+    y_train_vec = enc.transform(labels).toarray()
+    decoder = dict(enumerate(enc.categories_[0].flatten()))
+
+
+    # Detect Poison Using Activation Defence
+    defence = ChenActivations(model, x_train, y_train_vec, args)
+    report, is_clean_lst = defence.detect_poison(nb_clusters=2,
+                                                 nb_dims=3,
+                                                 reduce="PCA")
+
+    print("Analysis completed. Report:")
+    pp = pprint.PrettyPrinter(indent=10)
+    pprint.pprint(report)
+
+
+    # Evaluate Defense
+    # Evaluate method when ground truth is known:
+    print("------------------- Results using size metric -------------------")
+    # is_poison_train = 0 #THIS OVERWROTE SOMETHING IN AN UNDESIRABLE WAY SB
+    is_clean = (is_poison_train == 0)
+
+    confusion_matrix = defence.evaluate_defence(is_clean)
+
+    jsonObject = json.loads(confusion_matrix)
+    for label in jsonObject:
+        print(label)
+        pprint.pprint(jsonObject[label])
+
+    return confusion_matrix, jsonObject
